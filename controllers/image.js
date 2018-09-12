bedde8b1f2ddf9bcb3f52919f6617ff820849da8
Vincent Tam <vtam2012@gmail.com>
2018-09-11 21:53:18 -0700

    removed API KEY

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-  apiKey: 'cfc303b78cfe4f839da7b9ac6dc33ea9'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
