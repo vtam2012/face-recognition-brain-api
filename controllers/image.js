b28ad41dfb882ffff391bc7ebc56e86c551de353
Vincent Tam <vtam2012@gmail.com>
2018-09-11 21:27:05 -0700

    fixed sytax error

--- controllers/image.js
+++ controllers/image.js
@@ -23,7 +23,7 @@ const handleImage = (req, res, db) => {
   .catch(err => res.status(400).json('unable to get entries'))
 }
 
-module.exports - {
+module.exports = {
   handleImage: handleImage,
   handleApiCall: handleApiCall
 }
