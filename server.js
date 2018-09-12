49f32199dd1466da0b7251f2ca0b1c01f56a5a53
Vincent Tam <vtam2012@gmail.com>
2018-09-11 18:34:34 -0700

    initialize repo

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    user: 'Vince',
-    password: 'zdjirgmq',
+    user: 'postgres',
+    password: 'none',
     database: 'smart-brain'
   }
 });
